83de465e568b799a766796072a2d69d08573b722
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-02-26 23:32:59 -0500

    setup typescript and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  jsxA11y.flatConfigs.recommended,
+];
